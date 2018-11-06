8d6b07903e403b062e29268bace35d399959f296
David Lee <edenlhn@gmail.com>
2018-11-06 11:07:35 -0800

    Succeeded rendering all four modules together

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is listening at: http://localhost:${port}`);
