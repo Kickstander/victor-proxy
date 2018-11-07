641cad9d9ec5bb60205f5867b63cb26fe7a7696f
David Lee <edenlhn@gmail.com>
2018-11-06 23:55:35 -0800

    All modules up to date and moved to one folder

---
+++ proxy-server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at: http://localhost:${port}`);
+});
